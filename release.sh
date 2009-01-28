b3d44bb73e22e40c00e08fa9f0b241a450d5e6ef
piotr.tabor <piotr.tabor@b81c5082-ead4-11dd-a893-8d7ce9fbef62>
2009-01-28 21:44:25 +0000

    Release script

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VERSION=$1
+
+svn rm --force distr/latest_stable/*
+mkdir -p distr/latest_stable
+svn add distr/latest_stable
+cp -r trunk/netbeans-protobuf-plugin-suite/build/updates distr/$VERSION
+rm -rf distr/$VERSION/.svn
+cp trunk/netbeans-protobuf-plugin-suite/build/updates/* distr/latest_stable/
+svn add distr/$VERSION
+svn add distr/latest_stable/*
+
