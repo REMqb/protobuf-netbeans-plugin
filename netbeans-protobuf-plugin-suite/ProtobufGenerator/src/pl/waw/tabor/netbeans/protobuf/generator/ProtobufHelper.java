576c63072226919e595fbf11aaf756fb1ff23ac8
piotr.tabor <piotr.tabor@b81c5082-ead4-11dd-a893-8d7ce9fbef62>
2009-04-06 18:32:13 +0000

    Version 1.0.3. Fixed bug: #1 (http://code.google.com/p/protobuf-netbeans-plugin/issues/detail?id=1).

---
+++ netbeans-protobuf-plugin-suite/ProtobufGenerator/src/pl/waw/tabor/netbeans/protobuf/generator/ProtobufHelper.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pl.waw.tabor.netbeans.protobuf.generator;
+
+import java.io.File;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.loaders.DataObject;
+import org.openide.nodes.Node;
+
+/**
+ *
+ * @author ptab
+ */
+public class ProtobufHelper {
+
+    /**
+     * Find a file (one of files the protoc was runned on)
+     *
+     * @param fileName
+     * @return DataObject connected to the fileName or NULL if not found.
+     */
+    public static DataObject findDataObjectForFile(String fileName,Node[] nodes) {
+        for(Node node:nodes)
+        {
+            DataObject d=(DataObject)node.getCookie(DataObject.class);
+            FileObject fileObject=d.getPrimaryFile();
+            File file = FileUtil.toFile(fileObject);
+            if (file.toString().endsWith(fileName))
+                return d;
+        }
+        return null;
+    }
+}
