4dcf5d24c5c8587d4da750bfe1c1111056165e9a
piotr.tabor <piotr.tabor@b81c5082-ead4-11dd-a893-8d7ce9fbef62>
2010-06-21 22:59:36 +0000

    Tagging version 2.1.1

--- release.sh
+++ release.sh
@@ -2,12 +2,17 @@
 
 VERSION=$1
 
-svn rm --force distr/latest_stable/*
-mkdir -p distr/latest_stable
-svn add distr/latest_stable
-cp -r trunk/netbeans-protobuf-plugin-suite/build/updates distr/$VERSION
-rm -rf distr/$VERSION/.svn
-cp trunk/netbeans-protobuf-plugin-suite/build/updates/* distr/latest_stable/
-svn add distr/$VERSION
-svn add distr/latest_stable/*
+if [ "$VERSION" == "" ]; then
+	exit
+else 
+	svn rm --force distr/latest_stable/*
+	mkdir -p distr/latest_stable
+	svn add distr/latest_stable
+	cp -r trunk/netbeans-protobuf-plugin-suite/build/updates distr/$VERSION
+	rm -rf distr/$VERSION/.svn
+	cp trunk/netbeans-protobuf-plugin-suite/build/updates/* distr/latest_stable/
+	svn add distr/$VERSION
+	svn add distr/latest_stable/*
 
+	echo "Remember to tag: $VERSION"
+fi
